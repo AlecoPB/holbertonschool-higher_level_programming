0af9003fa6578541098f00e2ca4be120b9ecd24b
AlecoPB <ajoaquinpg@gmail.com>
2023-11-21 06:32:59 -0800

    Program that gives the last digit of a number and tells you its position from 1 to 10

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pythin3
+import random
+
+number = random.randint(-10000, 10000)
+dgt = str(number)[len(str(number)]
+if dgt > 5:
+                  indic = "and is greater than 5"
+elif dgt == 0:
+                  indic = "and is 0"
+else:
+                  indic = "and is less than 6 and not 0"
+print ("Last digit of", number, "is", dgt, indic)

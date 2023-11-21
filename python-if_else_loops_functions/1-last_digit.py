6bf93ed5060314d79aae102f0dc7ef01fb490a92
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-21 16:30:16 +0100

    Update 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 
 number = random.randint(-10000, 10000)
 dgt = str(number)[len(str(number))-1]
-#dgt = int(dgt)
-if number < 0: 
+# dgt = int(dgt)
+if number < 0:
     dgt = -1*int(dgt)
 if int(dgt) > 5:
     indic = "and is greater than 5"

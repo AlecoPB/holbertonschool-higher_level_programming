5dfd9cd571fff96b1aa98d05f801b63545853b7f
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-22 14:40:56 +0100

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(100):
+for i in range(1, 100):
     if i < 10:
         print("{:02}".format(i), end=", ")
     elif str(i)[0] != str(i)[1] and int(str(i)[0]) < int(str(i)[1]):

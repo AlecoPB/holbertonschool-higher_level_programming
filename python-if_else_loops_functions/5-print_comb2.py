66737a7acda8cf98bec1586c4370926ef077c647
AlecoPB <ajoaquinpg@gmail.com>
2023-11-22 01:32:15 -0800

    Prints 0..99 separated by commas

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        print("0{}".format(i), end=", ")
-    else:
-        print("{}".format(i), end=", ")
+print(", ".join("0{}".format(i) if i < 10 else "{}".format(i) for i in range (100)))

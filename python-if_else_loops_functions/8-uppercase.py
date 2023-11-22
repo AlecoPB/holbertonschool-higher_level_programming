57f9b0c4d385ba4416e835db3c166b597cc902a5
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-22 15:46:51 +0100

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def uppercase(c):
     for i in c:
-      print("{}".format(chr(ord(c[i])-32)) if 97 <= ord(c[i]) <= 123 else c[i], end='')
+      if 97 <= ord(c[i]) <= 123:
+        booly = True
+      else:
+        booly = False
+      print("{}".format(chr(ord(c[i])-32)) if booly else "{}".format(c[i]), end='')
     print()

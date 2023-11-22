7b821c141e2e03933660611e2a419fb42c351101
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-22 16:00:29 +0100

    Update 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(str(number)[len(str(number))-1], end='')
+    print(str(int(number))[len(str(number))-1], end='')
     return int(str(number)[len(str(number))-1])

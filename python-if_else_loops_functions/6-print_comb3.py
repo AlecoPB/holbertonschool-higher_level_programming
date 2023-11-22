e73b638645f527fa944976f5e9ca85390eec47d6
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-22 14:43:36 +0100

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -6,4 +6,4 @@ for i in range(1, 100):
         if i != 89:
             print("{}".format(i), end=", ")
         else:
-            print("{}".format(i), end=" ")
+            print("{}".format(i))

a320aed3bbde6e14c9e8c6619102cac936412a2a
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-23 10:52:15 +0100

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -10,8 +10,4 @@ class Square:
         """
         This is method documentation
         """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be greater than 0')
         self.__size=size

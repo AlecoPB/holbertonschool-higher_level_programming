0bfbd029f5818cf646fb1ff54bfecc44cc495512
AlecoPB <ajoaquinpg@gmail.com>
2023-11-23 01:37:35 -0800

    Added sized square

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is module documentation
+"""
+class Square:
+    """
+    This is class documentation
+    """
+    def __init__(self):
+        """
+        This is method documentation
+        """
+        self.__size=10

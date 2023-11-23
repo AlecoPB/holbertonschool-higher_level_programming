ff790ba871666a688d3b66c42cfdcc9b4390b9c7
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-23 10:50:45 +0100

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,4 +7,8 @@ class Square:
     This is class documentation
     """
     def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size <= 0:
+            raise ValueError('size must be greater than 0')
         self.__size=size

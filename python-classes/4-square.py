8a600d23d8580ba0a5fa4e6d6c4b6ed8cddf31e1
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-23 11:32:24 +0100

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,12 +8,15 @@ class Square:
     """
     def __init__(self, size=0):
         self.__size=size
+
+    @property
     def size(self):
         """
         This is method documentation
         """
         return self.__size
-        
+
+    @size.setter
     def size(self, value):
         """
         This is also method documentation

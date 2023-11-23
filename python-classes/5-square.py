87f1d9719d12434253b0017cf717724d45e26d37
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-23 11:57:25 +0100

    Create 5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+This is module documentation
+"""
+class Square:
+    """
+    This is class documentation
+    """
+    def __init__(self, size=0):
+        self.__size=size
+
+    @property
+    def size(self):
+        """
+        This is method documentation
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        This is also method documentation
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size=value
+        
+    def area(self):
+        """
+        Can you guess what this is?
+        """
+        return self.__size**2
+
+    def my_print(self):
+        """
+        Wowowow what is this?
+        """
+        side = self.__size
+        for i in range(side):
+          for j in range(side):
+            print('#')

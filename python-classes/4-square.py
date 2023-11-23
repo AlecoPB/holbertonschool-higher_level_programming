1566a0a25e9e45e2dcefe9b5b78d240e155c50b4
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-23 11:48:34 +0100

    Update 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -21,11 +21,11 @@ class Square:
         """
         This is also method documentation
         """
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        if size < 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
-        self.__size=size
+        self.__size=value
         
     def area(self):
         """

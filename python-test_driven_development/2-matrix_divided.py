86dcfd2609c024b6fc365f7bec6eb8dbdac5a414
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-27 10:25:47 +0100

    Update 2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,16 +2,18 @@
 '''
 Module documentation
 '''
+
+
 def matrix_divided(matrix, div):
     '''
     Function documentation
     '''
-  for i in range(len(matrix)):
-      if not isinstance(matrix[i], list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-      for j in range(len(matrix[i])):
-        if not isinstance(matrix[i][j], int) and not isinstance(matrix[i][j], float):
-          raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for i in range(len(matrix)):
+        if not isinstance(matrix[i], list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for j in range(len(matrix[i])):
+            if not isinstance(matrix[i][j], int) and not isinstance(matrix[i][j], float):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not isinstance(div, int) and not isinstance(div, float):
       raise TypeError("div must be a number")
     elif div == 0:

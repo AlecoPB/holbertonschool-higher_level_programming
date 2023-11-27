a6d199927a3e2d93ea3ec6ea31f6a4d294b5b9ea
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-27 12:01:48 +0100

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,6 +11,9 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     text = text.replace("? ", "?\n\n")
+    text = text.replace("?", "?\n\n")
     text = text.replace(". ", ".\n\n")
+    text = text.replace(".", ".\n\n")
     text = text.replace(": ", ":\n\n")
-    print(text)
+    text = text.replace(":", ":\n\n")
+    print(text, end="")

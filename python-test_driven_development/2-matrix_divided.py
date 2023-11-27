c6b0a361bf07b2c2ca88cc1418c5191b6c248540
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-27 10:29:29 +0100

    Update 2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,4 +27,4 @@ def matrix_divided(matrix, div):
                 raise TypeError("Each row of the matrix\
                       must have the same size")
     return [[round(matrix[i][j]/div, 2) for j in range(len(matrix[i]))]\
-           for i in range(len(matrix))]
+            for i in range(len(matrix))]

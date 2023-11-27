bcec53f0bb06918bb11d244631440116fd18d3b9
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-27 10:44:27 +0100

    Create 3-say_my_name.py

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module documentation
+"""
+
+
+def say_my_name(first_name, last_name=""):
+  """
+  Function documentation
+  """
+  if not isinstance(first_name, str):
+    raise TypeError("first_name must be a string")
+  elif not isinstance(last_name, str):
+    raise TypeError("last_name must be a string")
+  print("My name is {} {}".format(first_name, last_name))

10ed38ad7f8afedea361b2348496952566134796
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-27 11:03:36 +0100

    Create 4-print_square.py

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is module documentation
+"""
+
+
+def print_square(size):
+  if not isinstance(size, int):
+    raise TypeError("size must be an integer")
+  elif size < 0:
+    raise ValueError("size must be >= 0")
+  for i in range(size):
+    print("#" * size)

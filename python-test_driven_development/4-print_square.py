b842ff0238dc9cfcc679705b93a72c9a5e7befce
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-27 11:12:09 +0100

    Update 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,9 +5,9 @@ This is module documentation
 
 
 def print_square(size):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        for i in range(size):
-            print("#" * size)
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)

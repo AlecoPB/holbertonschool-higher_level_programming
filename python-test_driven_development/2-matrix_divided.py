48a1d5c0c29921049402b6e701a0089975d5458e
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-27 10:05:36 +0100

    Update 2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+'''
+Module documentation
+'''
 def matrix_divided(matrix, div):
+  '''
+  Function documentation
+  '''
   for i in range(len(matrix)):
     if not isinstance(matrix[i], list):
       raise TypeError("matrix must be a matrix (list of lists) of integers/floats")

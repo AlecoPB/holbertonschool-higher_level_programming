274028c4b534fd528e974ded1c445e0915b95311
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-30 15:11:36 +0100

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,6 +1,9 @@
 -- Task: print a description for the table first_table from hbtn_0c_0
 SELECT 
-    *    
+    COLUMN_NAME,
+    DATA_TYPE,
+   IS_NULLABLE,
+   COLUMN_DEFAULT
   FROM 
     INFORMATION_SCHEMA.COLUMNS
   WHERE

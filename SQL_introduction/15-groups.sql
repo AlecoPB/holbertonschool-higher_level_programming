16c51cf7db9f93f1b498f2f038f424cbbff4b465
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-30 16:21:06 +0100

    Update 15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- Task: List all the number of records with the same score
-SELECT COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+

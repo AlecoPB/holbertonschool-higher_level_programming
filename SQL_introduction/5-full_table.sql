585d8b8302ceae188c5611ea188be7d546ddb400
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-30 15:26:44 +0100

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,8 @@
 -- Task: print a description for the table first_table from hbtn_0c_0
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SELECT
+  COLUMN_NAME,
+FROM
+  INFORMATION_SCHEMA.COLUMNS
+WHERE
+  TABLE_SCHEMA = 'hbtn_0c_0' AND
+  TABLE_NAME = 'first_table' ;

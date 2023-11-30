6c0e92343823fd23ee68abb7d4f6c70e3ec23fb0
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-11-30 15:57:03 +0100

    Update 9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,13 +5,33 @@ CREATE TABLE IF NOT EXISTS second_table(
   score INT
 );
 
--- Insert the data
+-- Task: create a second_table and add rows
+CREATE TABLE IF NOT EXISTS second_table(
+  id INT,
+  name VARCHAR(256),
+  score INT
+);
+
 INSERT INTO second_table (
   id, name, score
   ) VALUES(
-  (1, "John", 10),
-  (2, "Alex", 3),
-  (3, "Bob", 14),
-  (4, "George", 8)
+  1, "John", 10
 );
 
+INSERT INTO second_table (
+  id, name, score
+  ) VALUES(
+  2, "Alex", 3
+);
+
+INSERT INTO second_table (
+  id, name, score
+  ) VALUES(
+  3, "Bob", 14
+);
+
+INSERT INTO second_table (
+  id, name, score
+  ) VALUES(
+  4, "George", 8
+);

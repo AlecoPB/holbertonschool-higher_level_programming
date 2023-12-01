6df5ad46e684cd683627045e1bad9a408a29766a
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-01 14:39:20 +0100

    Update 2-script.js

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,1 +1,5 @@
-
+var header = document.querySelector('header');
+var red_header = document.querySelector('#red_header');
+red_header.addEventListener("click", () => {
+  header.classList.add('red')
+});

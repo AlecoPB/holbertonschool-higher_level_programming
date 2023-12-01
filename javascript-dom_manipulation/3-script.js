330bbe0a1b90ab7f7aed253edcc3de46bc85da0e
AlecoPB <151534381+AlecoPB@users.noreply.github.com>
2023-12-01 14:56:38 +0100

    Update 3-script.js

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,1 +1,7 @@
-
+let header = document.querySelector('header');
+let toggle_header = document.querySelector('#toggle_header');
+//header.classList.add('red');
+toggle_header.addEventListener("click", () => {
+  header.classList.toggle('red')
+  header.classList.toggle('green')
+});
